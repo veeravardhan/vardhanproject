d2de0e09c8a80ad935723a5ff127e70a059b21e7
vardhan karnati <vadhanreddy1995@gmail.com>
2020-03-11 09:58:40 +0530

    third commit

--- src/Form.js
+++ src/Form.js
@@ -1,62 +1,128 @@
 import React from 'react';
 import axios from 'axios';
-import Button from 'react-bootstrap/Button'
-import form from 'react-bootstrap/Form'
-import "./Form.css"
+import { Form, Button } from 'react-bootstrap';
+import { Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import "./Form.css" 
 
 
-class Form extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value})
-      }
-    
-      handleSubmit(event) {
-        event.preventDefault();
-        const usname = {
-          name : this.state.value
-        }
-        axios.post('http://localhost:3001/user/add',usname)
-        .then((res) => {
-        console.log(res)
-        this.props.history.push("/User")
-        })
-        .catch(err => console.log(err))
-      }
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    render(){
-    return (
-        <div class="form-class">
-     <form>
-  <form.Group controlId="formBasicEmail">
-    <form.Label>Email address</form.Label>
-    <form.Control type="email" placeholder="Enter email" />
-    <form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </form.Text>
-  </form.Group>
-
-  <form.Group controlId="formBasicPassword">
-    <form.Label>Password</form.Label>
-    <form.Control type="password" placeholder="Password" />
-  </form.Group>
-  <form.Group controlId="formBasicCheckbox">
-    <form.Check type="checkbox" label="Check me out" />
-  </form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</form>
-        </div>
-    )
+  handleSubmit(values) {
+    const usname = {
+      name : values.name
     }
+    axios.post('http://localhost:3001/user/add',usname)
+    .then((res) => {
+    console.log(res)
+    this.props.history.push("/User")
+    })
+    .catch(err => console.log(err))
+  }
+
+render(){
+  const phoneRegExp = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
+
+// Schema for yup
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+  .min(2, "*Names must have at least 2 characters")
+  .max(100, "*Names can't be longer than 100 characters")
+  .required("*Name is required"),
+  email: Yup.string()
+  .email("*Must be a valid email address")
+  .max(100, "*Email must be less than 100 characters")
+  .required("*Email is required"),
+  phone: Yup.string()
+  .matches(phoneRegExp, "*Phone number is not valid")
+  .required("*Phone number required"),
+});
+
+  return(
+    <div className="form-class">
+    <Formik
+      initialValues={{ name:"", email:"", phone:""}}
+      validationSchema={validationSchema}
+      onSubmit={(values, {setSubmitting, resetForm}) => {
+          // When button submits form and form is in the process of submitting, submit button is disabled
+          setSubmitting(true);
+
+          // Simulate submitting to database, shows us values submitted, resets form
+            this.handleSubmit(values)
+            resetForm();
+            setSubmitting(false);
+          
+      }}
+    >
+      {( {values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting }) => (
+        <Form onSubmit={handleSubmit} className="mx-auto">
+          <Form.Group controlId="formName">
+            <Form.Label>Name :</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Full Name"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.name}
+              className={touched.name && errors.name ? "has-error" : null}
+              />
+              {touched.name && errors.name ? (
+                <div className="error-message">{errors.name}</div>
+              ): null}
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email :</Form.Label>
+            <Form.Control
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              className={touched.email && errors.email ? "has-error" : null}
+               />
+               {touched.email && errors.email ? (
+                 <div className="error-message">{errors.email}</div>
+               ): null}
+          </Form.Group>
+          <Form.Group controlId="formPhone">
+            <Form.Label>Phone :</Form.Label>
+            <Form.Control
+              type="text"
+              name="phone"
+              placeholder="Phone"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.phone}
+              className={touched.phone && errors.phone ? "has-error" : null}
+               />
+             {touched.phone && errors.phone ? (
+                 <div className="error-message">{errors.phone}</div>
+               ): null}
+          </Form.Group>
+          {/*Submit button that is disabled after button is clicked/form is in the process of submitting*/}
+          <Button variant="primary" type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+    </div>
+  );
+}
 }
 
-export default Form;
+
+
+export default MyForm;
